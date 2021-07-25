6ef140e1cece40b0a047548f2b0e6dadf4a9edd0
Md Azim <mdazim.cse@gmail.com>
2021-07-25 22:33:26 +0600

    done

---
+++ src/Components/TodoTask.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { ITask } from "../Interfaces";
+
+interface Props {
+  task: ITask;
+  completeTask(taskNameToDelete: string): void;
+}
+
+const TodoTask = ({ task, completeTask }: Props) => {
+  return (
+    <div className="task">
+      <h1>State</h1>
+      <div className="content">
+        <span>{task.taskName}</span>
+        <span>{task.deadline}</span>
+      </div>
+      <button
+        onClick={() => {
+          completeTask(task.taskName);
+        }}
+      >
+        DELETE
+      </button>
+    </div>
+  );
+};
+
+export default TodoTask;
